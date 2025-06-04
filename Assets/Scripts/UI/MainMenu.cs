896bdf05d51038bf1ba12390897edcb51571cde3
Maura Dotson <dotsmaura666@gmail.com>
2025-06-04 18:04:39 -0400

    just added comments and working on the Settings "page"

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -7,9 +7,11 @@ public class MainMenuScript : MonoBehaviour
 {
     public void PlayButton()
     {
+        //when play is pressed, the game will start. currently is using a temp scene name
         SceneManager.LoadScene("SampleScene");
     }
 
+    //not really needed, but its for the settings page
     public void SettingsButton()
     {
         SceneManager.LoadScene("Settings");
