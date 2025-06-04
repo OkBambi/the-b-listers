6ddb5291eef634cde6282a1124faa296421b63a8
Maura Dotson <dotsmaura666@gmail.com>
2025-06-04 15:41:06 -0400

    Main Menu baser + UI folder
    Creating a base main menu at the moment, will be fledge out soon. Play and Quit button are added. Settings button will be created soon.
    Created a UI folder since there wasn't one.
    TextMesh has been added to.

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Test");
+    }
+
+    public void SettingsButton()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void QuitButton()
+    {
+        Debug.Log("Quitting..");
+        Application.Quit();
+    }
+
+}
