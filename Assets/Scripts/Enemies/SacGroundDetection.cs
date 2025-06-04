efc2d1fd7d41db07b0c1dc969ba2626696bcb5b7
LanceCoolie2018 <Tmansornig@Gmail.com>
2025-06-04 15:36:41 -0400

    destroy sac on impact with ground

--- Assets/Scripts/Enemies/SacGroundDetection.cs
+++ Assets/Scripts/Enemies/SacGroundDetection.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class SacGroundDetection : MonoBehaviour
 {
-    string groundTag = "Ground";
+    [SerializeField] Renderer model;
+  
+    string groundTag = "groundTag";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("test check");
         if (collision.gameObject.tag == groundTag)
         {
             Debug.Log("Ground detected");
+            Destroy(gameObject);
         }
     }
 }
