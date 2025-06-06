a75a4ea5e5c4485b49a1e3294ebdb6ccf01699a2
BrandonCrespo <102543971+BrandonCrespo@users.noreply.github.com>
2025-06-06 15:01:42 -0400

    building BlueSchmove from the ground up
    made it so the Sticky sticks to an object.

--- Assets/Scripts/Player/SchmoveScripts/BlueSchmove.cs
+++ Assets/Scripts/Player/SchmoveScripts/BlueSchmove.cs
@@ -24,6 +24,7 @@ public class BlueSchmove : MonoBehaviour
     void Start()
     {
         activated = true;//test for now
+        rb.linearVelocity = new Vector3(0,0,stickySpeed);
     }
 
     // Update is called once per frame
@@ -31,42 +32,75 @@ public class BlueSchmove : MonoBehaviour
     {
         if (activated)
         {
-            RaycastHit hit;
-            if (!attached)
-            {
-                rb.AddForce(transform.forward * stickySpeed * Time.deltaTime, ForceMode.Impulse);
-                if (Physics.Raycast(rb.transform.position, rb.transform.forward, out hit, 1, ~ignoreLayer))//distance is just that it needs to be close
-                {
-                    attached = true;
-                    holderRb = rb;
-                    //rb.isKinematic = false;
-                    rb.transform.parent = hit.transform;
-                }
-            }
-            else
-            {
-                if (Physics.SphereCast(holderRb.transform.position, pulseRadius, holderRb.transform.forward, out hit, pulseRadius, ~ignoreLayer))
-                {
-                    IDamage dmg = hit.collider.GetComponent<IDamage>();
-                    StickToHit();
-                    if (dmg != null)
-                    {
-                        Debug.Log("NO");
-                        dmg.takeDamage(PrimaryColor.OMNI, 2);
-                    }
-                }
-                isStuck = true;
-                StartCoroutine(Pulse());
-                activated = false;
-                attached = false;
-            }
+            //if (!attached)
+            //{
+            //    RaycastHit hit;
+            //    if (Physics.SphereCast(rb.transform.position, rb.transform.localScale.x, rb.transform.forward, out hit, 1, ~ignoreLayer))//distance is just that it needs to be close
+            //    {
+            //        attached = true;
+            //        rb.linearVelocity = new Vector3(0, 0, 0);
+            //        rb.transform.SetParent(hit.transform);
+            //    }
+            //}
+            //else
+            //{
+
+            //}
+            //if (attached)
+            //{
+            //    rb.linearVelocity = new Vector3(0, 0, 0);
+            //}
+            //RaycastHit hit;
+            //if (!attached)
+            //{
+            //    rb.AddForce(transform.forward * stickySpeed * Time.deltaTime, ForceMode.Impulse);
+            //    if (Physics.Raycast(rb.transform.position, rb.transform.forward, out hit, 1, ~ignoreLayer))//distance is just that it needs to be close
+            //    {
+            //        attached = true;
+            //        holderRb = rb;
+            //        //rb.isKinematic = false;
+            //        rb.transform.parent = hit.transform;
+            //    }
+            //}
+            //else
+            //{
+            //    if (Physics.SphereCast(holderRb.transform.position, pulseRadius, holderRb.transform.forward, out hit, pulseRadius, ~ignoreLayer))
+            //    {
+            //        IDamage dmg = hit.collider.GetComponent<IDamage>();
+            //        StickToHit();
+            //        if (dmg != null)
+            //        {
+            //            Debug.Log("NO");
+            //            dmg.takeDamage(PrimaryColor.OMNI, 2);
+            //        }
+            //    }
+            //    isStuck = true;
+            //    StartCoroutine(Pulse());
+            //    activated = false;
+            //    attached = false;
+            //}
         }
-        if (isStuck)
-        {
-            rb.transform.position = rb.transform.parent.localPosition;
-        }
+        //if (isStuck)
+        //{
+        //    rb.transform.position = rb.transform.parent.localPosition;
+        //}
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("does it work");
+        if (other.isTrigger)
+        {
+            return;
+        }
+        Stuck();
+    }
+
+    public void Stuck()
+    {
+        isStuck = true;
+        rb.linearVelocity = new Vector3(0, 0, 0);
+    }
 
     public void Activate()
     {
---
+++ Assets/Scripts/StickToObject.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem.HID;
+
+public class StickToObject : MonoBehaviour
+{
+    bool stuck;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        stuck = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger || stuck)
+        {
+            return;
+        }
+        transform.SetParent(other.transform);
+        stuck = true;
+        Rigidbody rb = transform.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.linearVelocity = Vector3.zero;
+    }
+}
