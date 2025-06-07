d107a2680c9cbf06c693696552774360686e8d5f
Maura Dotson <dotsmaura666@gmail.com>
2025-06-07 18:49:50 -0400

    Updated Res code. Still hates the dropdown

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -19,14 +19,24 @@ public class SettingsMenu : MonoBehaviour
         resolutionDropdown.ClearOptions();  //clear out all the optins in the resolution dropdown
 
         List<string> options = new List<string>();  //creat a list of strings that will be our options
+
+        int currentResolutionIndex = 0;
         //loop for each element in our array
         for (int i = 0; i < resolutions.Length; i++)
         {
             //string option = "width" + " x " + "height";
             string option = resolutions[i].width + " x " + resolutions[i].height;   //a nicely formated string will be crated for them
             options.Add(option);    //then gets added to the list
+
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
         resolutionDropdown.AddOptions(options); //once done, it gets back added to the res dropdown
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
 
