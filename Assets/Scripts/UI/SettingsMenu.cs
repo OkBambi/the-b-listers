9d893ee19ff9ecb7fb66cf518f7ad9709d84344f
Maura Dotson <dotsmaura666@gmail.com>
2025-06-07 18:41:28 -0400

    Res coded. What you mean you won't accept the dropdown?

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -8,18 +8,25 @@ public class SettingsMenu : MonoBehaviour
 {
     //audio variable
 
+    //res stuff
     public Dropdown resolutionDropdown;
     Resolution[] resolutions;
-
 
     void Start()
     {
         //resolution
         resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();  //clear out all the optins in the resolution dropdown
 
-        resolutionDropdown.ClearOptions();
-
-
+        List<string> options = new List<string>();  //creat a list of strings that will be our options
+        //loop for each element in our array
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            //string option = "width" + " x " + "height";
+            string option = resolutions[i].width + " x " + resolutions[i].height;   //a nicely formated string will be crated for them
+            options.Add(option);    //then gets added to the list
+        }
+        resolutionDropdown.AddOptions(options); //once done, it gets back added to the res dropdown
     }
 
 
@@ -31,5 +38,4 @@ public class SettingsMenu : MonoBehaviour
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
-
 }
