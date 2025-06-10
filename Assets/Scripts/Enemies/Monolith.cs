e6303e95d96fa99b76d9d144e995edf2cf773254
BrandonCrespo <102543971+BrandonCrespo@users.noreply.github.com>
2025-06-10 09:10:12 -0400

    Monolith rotation
    Hopefully allows the monolith to rotate around the center of the map and spin on it's y-axis
    
    Co-Authored-By: Thomas Sornig <180203385+LanceCoolie2018@users.noreply.github.com>

--- Assets/Scripts/Enemies/Monolith.cs
+++ Assets/Scripts/Enemies/Monolith.cs
@@ -58,9 +58,10 @@ public class Monolith : EnemyBase
 
     void movement()
     {
+        //transform.RotateAround(Vector3.zero, Vector3.up,   * Time.deltaTime);//allows the monolith to spin around the center point
+        //transform.Rotate(new Vector3(0,  , 0) * Time.deltaTime);//allows the monolith to rotate around the y axis.
         posX = rb.position.x + Mathf.Cos(angle) * rotationRadius;
         posZ = rb.position.z + Mathf.Sin(angle) * rotationRadius;
-        transform.RotateAround(Vector3.zero, Vector3.up, rotationSpeed);//allows the monolith to spin around on the y-axis
         transform.position = new Vector3(posX, transform.position.y, posZ);
         angle += angularSpeed * Time.deltaTime;
 
