176398e53a1e1d4de26815d6b0a2381679b27667
BrandonCrespo <102543971+BrandonCrespo@users.noreply.github.com>
2025-06-13 15:25:17 -0400

    Update ComboFeed.cs

--- Assets/Scripts/UI/ComboFeed.cs
+++ Assets/Scripts/UI/ComboFeed.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class ComboFeed : MonoBehaviour
 {
+    public static ComboFeed theInstance;
+    [SerializeField] GameObject feedListingPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        theInstance = this;
+    }
+
+    public void AddNewComboFeed(string _modifier, string _score, string _killedOrUsed) //allows you to add to the kill feed (modifier is what is done to the score. Example + or -)
+    {
+        GameObject newScoreFeed = Instantiate(feedListingPrefab, transform);
+        newScoreFeed.transform.SetSiblingIndex(0);
+        FeedListing feedListing = newScoreFeed.GetComponent<FeedListing>();
+        feedListing.SetScoreAndHow(_modifier, _score, _killedOrUsed);
     }
 
     // Update is called once per frame
