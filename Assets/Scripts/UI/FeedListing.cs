4782277e96dfb2f4f172a44c849d4bbf9ec87d91
BrandonCrespo <102543971+BrandonCrespo@users.noreply.github.com>
2025-06-13 15:12:49 -0400

    Base for kill feed
    Added all the game objects needed for the score feed.

---
+++ Assets/Scripts/UI/FeedListing.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FeedListing : MonoBehaviour
+{
+    [SerializeField] Text modifier;
+    [SerializeField] Text scoreAmount;
+    [SerializeField] Text killedOrUsed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Destroy(gameObject, 5f);//kills feed if it lasts longer then 5 seconds.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetScoreAAndHow(string _modifier, string _scoreAmount, string _killedOrUsed)
+    {
+        modifier.text = _modifier;
+        scoreAmount.text = _scoreAmount;
+        killedOrUsed.text = _killedOrUsed;
+    }
+}
