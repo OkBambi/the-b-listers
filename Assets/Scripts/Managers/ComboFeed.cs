a4d30e49827512f9ba7a7286d023c4834a58772a
BrandonCrespo <102543971+BrandonCrespo@users.noreply.github.com>
2025-06-16 22:29:00 -0400

    Added a feed cap
    Allows the developer to set the amount of items shown in the feed.

--- Assets/Scripts/Managers/ComboFeed.cs
+++ Assets/Scripts/Managers/ComboFeed.cs
@@ -1,9 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ComboFeed : MonoBehaviour
 {
     public static ComboFeed theInstance;
     [SerializeField] GameObject feedListingPrefab;
+    [SerializeField] int maxFeedLength;
+
+    private Queue<GameObject> currentFeedList = new Queue<GameObject>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,8 +18,17 @@ public class ComboFeed : MonoBehaviour
     public void AddNewComboFeed(string _modifier, string _score, string _killedOrUsed) //allows you to add to the kill feed (modifier is what is done to the score. Example + or -)
     {
         GameObject newScoreFeed = Instantiate(feedListingPrefab, transform);
+        AddToQueue(newScoreFeed);
         newScoreFeed.transform.SetSiblingIndex(0);
-        FeedListing feedListing = newScoreFeed.GetComponent<FeedListing>();
-        feedListing.SetScoreAndHow(_modifier, _score, _killedOrUsed);
+        newScoreFeed.GetComponent<FeedListing>().SetScoreAndHow(_modifier, _score, _killedOrUsed);
+    }
+
+    private void AddToQueue(GameObject _newScoreFeed)
+    {
+        currentFeedList.Enqueue(_newScoreFeed);
+        if (currentFeedList.Count > maxFeedLength)
+        {
+            Destroy(currentFeedList.Dequeue());
+        }
     }
 }
