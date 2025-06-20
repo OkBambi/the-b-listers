883f8fd4c9ee6405a38290424629b21c2935f296
ILostAccessToYcGun <ycgun32@gmail.com>
2025-06-20 17:28:36 -0400

    Settings Manager should be fully setup
    I have not updated the prefabs to include them but I will shortly

--- Assets/Scripts/Managers/SettingsManager.cs
+++ Assets/Scripts/Managers/SettingsManager.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class SettingsManager : MonoBehaviour
 {
-    //basically this is the middleware to interact with the settings,
-    //including reading data from the object and writing data into the object
-    //(get and set)
+    /*
+     * To change settings, use the corresponding method and pass in the value you want the settings to change to
+        * E.g SettingsManager.instance.FOV(90);
+        
+     * To get settings, use the corresponding method and use the method's return value
+        * E.g int settingsFOV = SettingsManager.instance.FOV();
+     */
+
+    public static SettingsManager instance;
+
     [SerializeField] SettingsObject settings;
 
     #region Resolution
@@ -39,5 +46,83 @@ public class SettingsManager : MonoBehaviour
         settings.windowType = _windowType;
     }
     #endregion
+
+    #region FOV
+    public int FOV()
+    {
+        return settings.FOV;
+    }
+    public void FOV(int _FOV)
+    {
+        settings.FOV = _FOV;
+    }
+    #endregion
+
+    #region isReducedCameraShake
+    public bool isReducedCameraShake()
+    {
+        return settings.isReducedCameraShake;
+    }
+    public void isReducedCameraShake(bool _isReducedCameraShake)
+    {
+        settings.isReducedCameraShake = _isReducedCameraShake;
+    }
+    #endregion
+
+    #region isArcadeFilter
+    public bool isArcadeFilter()
+    {
+        return settings.isArcadeFilter;
+    }
+    public void isArcadeFilter(bool _isArcadeFilter)
+    {
+        settings.isArcadeFilter = _isArcadeFilter;
+    }
+    #endregion
+
+    #region isOutlineFilter
+    public bool isOutlineFilter()
+    {
+        return settings.isOutlineFilter;
+    }
+    public void isOutlineFilter(bool _isOutlineFilter)
+    {
+        settings.isOutlineFilter = _isOutlineFilter;
+    }
+    #endregion
+
+    #region isReducedCameraShake
+    public bool isColourBlindnessMode()
+    {
+        return settings.isColourBlindnessMode;
+    }
+    public void isColourBlindnessMode(bool _isColourBlindnessMode)
+    {
+        settings.isColourBlindnessMode = _isColourBlindnessMode;
+    }
+    #endregion
+
+    #region VFX
+    public float VFX()
+    {
+        return settings.VFX;
+    }
+    public void VFX(float _VFX)
+    {
+        settings.VFX = _VFX;
+    }
+    #endregion
+
+    #region mouseSensitivity
+    public float mouseSensitivity()
+    {
+        return settings.mouseSensitivity;
+    }
+    public void mouseSensitivity(float _mouseSensitivity)
+    {
+        settings.mouseSensitivity = _mouseSensitivity;
+    }
+    #endregion
+
 
 }
--- Assets/Scripts/Settings/SettingsObject.cs
+++ Assets/Scripts/Settings/SettingsObject.cs
@@ -3,20 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SettingsObject", menuName = "Settings", order = 1)]
 public class SettingsObject : ScriptableObject
 {
-    //public enum ResolutionTypes 
-    //{ 
-    //    _640x480, _800x600, _1024x768, _1152x864,
-    //    _1280x600, _1280x720, _1280x768, _1280x800, _1280x960, _1280x1024,
-    //    _1360x768, _1366x768, _1400x1050, _1440x900,
-    //    _1600x900, _1600x1200, _1680x1050,
-    //    _1792x1344, _1856x1392, _1920x1080, _1920x1200, _1920x1440,
-    //    _2048x1152, _2048x1536, _2560x1440, _2560x1600
-    //}
     public enum QualityTypes { Low, Medium, High }
     public enum WindowType { Windowed, Fullscreen, FullscreenWindowed }
 
     [Header("Graphics")]
-    
+
     public Resolution resolution;
     public QualityTypes quality;
     public WindowType windowType;
