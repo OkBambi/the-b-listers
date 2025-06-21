c48b845b7f7a5fcfe984a71737ec4ef38ac52d3d
BrandonCrespo <102543971+BrandonCrespo@users.noreply.github.com>
2025-06-21 17:30:42 -0400

    made the script for the color particles

---
+++ Assets/Scripts/World/ColorParticles.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class ColorParticles : MonoBehaviour
+{
+    public ParticleSystem colorParticles;
+    public GameObject splatsPrefab;
+    public Transform splatsHolder;
+    private List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ParticlePhysicsExtensions.GetCollisionEvents(colorParticles, other, collisionEvents);
+
+        int count = collisionEvents.Count;
+
+        for (int index = 0; index < count; index++)
+        {
+            GameObject theSplat = Instantiate(splatsPrefab, collisionEvents[index].intersection, Quaternion.identity);
+            theSplat.transform.SetParent(splatsHolder, true);
+            Splats splatsScript = theSplat.GetComponent<Splats>();
+            //splatsScript.Initialize(Splats.SplatLocation);
+        }
+    }
+}
