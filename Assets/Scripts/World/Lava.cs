92a28b2808100b602af5aa4b372c28cfa22b8162
OkBambi <ethoniadavis@gmail.com>
2025-06-22 18:02:22 -0400

    reworked Fallzone

--- Assets/Scripts/World/Lava.cs
+++ Assets/Scripts/World/Lava.cs
@@ -1,16 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class Lava : MonoBehaviour
 {
+    [SerializeField] Renderer model;
+    [SerializeField] int cooldown;
+    [SerializeField] bool isOnCooldown;
+    [SerializeField] Vector3 teleportPosition;
+
     private void OnTriggerEnter(Collider other)
     { 
 
         IDamage dmg = other.GetComponent<IDamage>();
 
-        if (dmg != null )
+        if (dmg != null && other.CompareTag("Player"))
         {
-            Debug.Log("Lava.");
-            dmg.takeDamage(PrimaryColor.OMNI, 100);
+            if (!isOnCooldown)
+            {
+                isOnCooldown = true;
+                model.material.color = Color.red;
+
+                other.transform.position = teleportPosition;
+
+                StartCoroutine(Cooldown());
+            }
+            else
+            {
+                dmg.takeDamage(PrimaryColor.OMNI, 100);
+            }
         }
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        model.material.color = Color.black;
+        isOnCooldown = false;
     }
 }
