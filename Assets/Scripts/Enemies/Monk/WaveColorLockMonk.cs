98be6b194e30b906d32f566ab9949a0ff43e31f3
Mortal-Atlas <jacobzgino@gmail.com>
2025-07-02 00:30:52 -0400

    colorlock update
    lock when hit by wave. it does not stop though.

--- Assets/Scripts/Enemies/Monk/WaveColorLockMonk.cs
+++ Assets/Scripts/Enemies/Monk/WaveColorLockMonk.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class WaveColorLockMonk : MonoBehaviour
@@ -5,13 +6,22 @@ public class WaveColorLockMonk : MonoBehaviour
     [SerializeField] GameObject WaveInstance;
     [SerializeField] float ColorLockTimer;
 
+    //animation
+    [SerializeField] GameObject ChainUI;
 
+    IEnumerator ChainScreen()
+    {
+        yield return new WaitForSeconds(ColorLockTimer);
+        ChainUI.SetActive(false);
+        Debug.Log("ChainUI Disabled");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         IColorLock colorLock = GameManager.instance.playerScript.GetComponent<IColorLock>();
-            colorLock.LockColorSelection(ColorLockTimer);
-
+        colorLock.LockColorSelection(ColorLockTimer);
+        ChainUI.SetActive(true);
+        StartCoroutine(ChainScreen());
     }
 
     private void OnTriggerExit(Collider other)
