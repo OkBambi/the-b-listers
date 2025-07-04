c3db6514be98c3839ee5f411b847c4dbfcddd697
Maura Dotson <dotsmaura666@gmail.com>
2025-07-03 23:26:39 -0400

    figuring stuff out

--- Assets/Scripts/UI/PopOut.cs
+++ Assets/Scripts/UI/PopOut.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PopOut : MonoBehaviour
+public class PopOut : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] Vector3 NormalPosition;
+    [SerializeField] Vector3 HoverPosition;
 
-    // Update is called once per frame
+    private bool mouse_over = false;
+
     void Update()
     {
-        
+        if (mouse_over)
+        {
+            Debug.Log("Mouse Over");
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouse_over = true;
+        Debug.Log("Mouse enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouse_over = false;
+        Debug.Log("Mouse exit");
     }
 }
