c5f34dc8669af1672e88a64536b525e3125e4882
Maura Dotson <dotsmaura666@gmail.com>
2025-07-04 00:02:32 -0400

    Spasming icons

--- Assets/Scripts/UI/PopOut.cs
+++ Assets/Scripts/UI/PopOut.cs
@@ -3,28 +3,48 @@ using UnityEngine.EventSystems;
 
 public class PopOut : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    //public float liftHeight = 0.5f;
+    //[SerializeField] Vector3 originalPosition;
+    //private bool isHovering = false;
+
     [SerializeField] Vector3 NormalPosition;
     [SerializeField] Vector3 HoverPosition;
 
-    private bool mouse_over = false;
-
-    void Update()
+    void Start()
     {
-        if (mouse_over)
-        {
-            Debug.Log("Mouse Over");
-        }
+        NormalPosition = transform.position;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        mouse_over = true;
-        Debug.Log("Mouse enter");
+        // Move the card to the hover position when the mouse pointer enters
+        transform.position = HoverPosition;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        mouse_over = false;
-        Debug.Log("Mouse exit");
+        // Return the card to its normal position when the mouse pointer exits
+        transform.position = NormalPosition;
     }
+    //void Update()
+    //{
+    //    if (isHovering)
+    //    {
+    //        Debug.Log("Mouse Over");
+    //    }
+    //}
+
+    //public void OnPointerEnter(PointerEventData eventData)
+    //{
+    //    isHovering = true;
+    //    transform.Translate(Vector3.up * liftHeight);
+    //    Debug.Log("Mouse enter");
+    //}
+
+    //public void OnPointerExit(PointerEventData eventData)
+    //{
+    //    isHovering = false;
+    //    transform.Translate(Vector3.down * liftHeight);
+    //    Debug.Log("Mouse exit");
+    //}
 }
