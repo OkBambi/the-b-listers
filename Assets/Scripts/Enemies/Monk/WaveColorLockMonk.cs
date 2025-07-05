9831d2fd1bd76ce703c9ca62ad81465961ed027b
Mortal-Atlas <jacobzgino@gmail.com>
2025-07-05 19:43:08 -0400

    fix monk chains - now modular

--- Assets/Scripts/Enemies/Monk/WaveColorLockMonk.cs
+++ Assets/Scripts/Enemies/Monk/WaveColorLockMonk.cs
@@ -1,20 +1,30 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class WaveColorLockMonk : MonoBehaviour
 {
     [SerializeField] public int ColorLockTimer;
-    [SerializeField] GameObject ChainLock;
-    [SerializeField] GameObject ChainUnlock;
-    [SerializeField] GameObject ChainUI;
+    [SerializeField] ChainMarker[] ChainStates;
+    [SerializeField] RawImage[] ChainToggleables;
     [SerializeField] LockColorChange LockColorChange;
+
+    private void Start()
+    {
+        //Find the object with chain marker script on the scene
+        ChainStates = FindObjectsByType<ChainMarker>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        //add Images to ChainImageArray
+        ChainToggleables = new RawImage[2] { ChainStates[0].GetComponent<RawImage>(), ChainStates[1].GetComponent<RawImage>() };
+
+    }
 
 
     public void ChainScreen()
     {
-        ChainLock.SetActive(true);
-        ChainUnlock.SetActive(false);
+        ChainStates[1].gameObject.SetActive(true);
+        ChainStates[0].gameObject.SetActive(false);
         AudioManager.instance.Play("Monk_Wave_Hit");
         StartCoroutine(ExitChainScreen(ColorLockTimer));
     }
@@ -25,8 +35,8 @@ public class WaveColorLockMonk : MonoBehaviour
         Debug.Log("BEFORE EXIT CHAIN SCREEN TRIGGER");
         yield return new WaitForSeconds(timer);
         AudioManager.instance.Play("Monk_Wave_End");
-        ChainUnlock.SetActive(true);
-        ChainLock.SetActive(false);
+        ChainStates[0].gameObject.SetActive(true);
+        ChainStates[1].gameObject.SetActive(false);
 
 
         Debug.Log("IVE GONE THROUGH IT, IT SHOULD WORK");
