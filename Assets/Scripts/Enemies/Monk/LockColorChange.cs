f9e873ac4d418d3c33417a81d0a0693262318bb8
Mortal-Atlas <jacobzgino@gmail.com>
2025-07-09 22:47:12 -0400

    Comfortable Here But need a whole load of work done
    Reverted back to its old version where it works. Not hopeful for what is to come. but I will do my best

--- Assets/Scripts/Enemies/Monk/LockColorChange.cs
+++ Assets/Scripts/Enemies/Monk/LockColorChange.cs
@@ -5,13 +5,11 @@ using UnityEngine.Video;
 public class LockColorChange : MonoBehaviour
 {
     [SerializeField] Monk MonkPrimary;
-    [SerializeField] ChainMarker[] ChainStates;
-    [SerializeField] RawImage[] ChainToggleables;
+    [SerializeField] ChainMarker[] ChainToggleables;
+    [SerializeField] RawImage[] ChainImageArray;
     [SerializeField] PrimaryColor PrimaryColor;
     [SerializeField] ColorSwapping ChangeColor;
     [SerializeField] WaveColorLockMonk ColorLockMonk;
-
-
 
 
     void Start()
@@ -21,21 +19,11 @@ public class LockColorChange : MonoBehaviour
         //find ColorSwapping Script in scene
         ChangeColor = FindFirstObjectByType<ColorSwapping>();
         //Find the object with chain marker script on the scene
-        ChainStates = FindObjectsByType<ChainMarker>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        ChainToggleables = FindObjectsByType<ChainMarker>(FindObjectsInactive.Include, FindObjectsSortMode.None);
 
-        ChainToggleables = new RawImage[2];
+        //add Images to ChainImageArray
+        ChainImageArray = new RawImage[2] { ChainToggleables[0].GetComponent<RawImage>(), ChainToggleables[1].GetComponent<RawImage>() };
 
-        foreach (ChainMarker chainMarker in ChainStates)
-        {
-            if (chainMarker.chainType == ChainType.Lock)
-            {
-                ChainToggleables[0] = chainMarker.GetComponent<RawImage>();
-            }
-            if (chainMarker.chainType == ChainType.Unlock)
-            {
-                ChainToggleables[1] = chainMarker.GetComponent<RawImage>();
-            }
-        }
     }
 
 
@@ -48,23 +36,22 @@ public class LockColorChange : MonoBehaviour
         switch (PrimaryColor)
         {
             case PrimaryColor.RED:
-                ChainToggleables[0].color = Color.red;
-                ChainToggleables[1].color = Color.red;
+                ChainImageArray[0].color = Color.red;
+                ChainImageArray[1].color = Color.red;
                 ChangeColor.SwapToColour(GameManager.instance.playerScript.GetPlayerColor(), PrimaryColor.RED, ref GameManager.instance.playerScript.currentColor);
                 break;
 
             case PrimaryColor.BLUE:
-                ChainToggleables[0].color = Color.blue;
-                ChainToggleables[1].color = Color.blue;
+                ChainImageArray[0].color = Color.blue;
+                ChainImageArray[1].color = Color.blue;
                 ChangeColor.SwapToColour(GameManager.instance.playerScript.GetPlayerColor(), PrimaryColor.BLUE, ref GameManager.instance.playerScript.currentColor);
                 break;
 
             case PrimaryColor.YELLOW:
-                ChainToggleables[0].color = Color.yellow;
-                ChainToggleables[1].color = Color.yellow;
+                ChainImageArray[0].color = Color.yellow;
+                ChainImageArray[1].color = Color.yellow;
                 ChangeColor.SwapToColour(GameManager.instance.playerScript.GetPlayerColor(), PrimaryColor.YELLOW, ref GameManager.instance.playerScript.currentColor);
                 break;
         }
     }
 }
-
