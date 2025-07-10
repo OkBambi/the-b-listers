5b15c601707afa7daafb040b468205ccaa83c528
BrandonCrespo <102543971+BrandonCrespo@users.noreply.github.com>
2025-07-10 17:46:31 -0400

    Able to get the highscores that were saved

--- Assets/Scripts/Managers/HighScoreManager.cs
+++ Assets/Scripts/Managers/HighScoreManager.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class HighScoreManager : MonoBehaviour
@@ -12,16 +14,29 @@ public class HighScoreManager : MonoBehaviour
         theInstance = this;
     }
 
-    public void SaveHighScore(float newHighScore)
+    public static void SaveHighScore(float newHighScore)
     {
         List<float> highScores = GetHighScores();
         highScores.Add(newHighScore);
-        highScores = highScores.OrderByDecending(s => s).Take(MaxHighScores).ToList();
+        highScores = highScores.OrderByDescending(s => s).Take(MaxHighScores).ToList();
 
         for (int index = 0; index < highScores.Count; index++)
         {
             PlayerPrefs.SetFloat(prefix + index, highScores[index]);
         }
         PlayerPrefs.Save();
+    }
+
+    public static List<float> GetHighScores()
+    {
+        List<float> highScores = new List<float>();
+        for (int index = 0; index < MaxHighScores; index++)
+        {
+            if(PlayerPrefs.HasKey(prefix + index))
+            {
+                highScores.Add(PlayerPrefs.GetFloat(prefix + index));
+            }
+        }
+        return highScores;
     }
 }
