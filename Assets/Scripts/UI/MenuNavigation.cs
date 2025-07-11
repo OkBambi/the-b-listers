7c38caefb6c55228bb22f67a252a702612d7029f
LanceCoolie2018 <Tmansornig@Gmail.com>
2025-07-11 15:14:51 -0400

    create find selectable function

--- Assets/Scripts/UI/MenuNavigation.cs
+++ Assets/Scripts/UI/MenuNavigation.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MenuNavigation : MonoBehaviour
 {
+    private GameObject selectedObject;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +17,22 @@ public class MenuNavigation : MonoBehaviour
     {
         
     }
+
+    GameObject FindNextSelectable(GameObject current, Vector2 direction)
+    {
+        Selectable currentSelectable = current.GetComponent<Selectable>();
+        if (currentSelectable == null) return null;
+
+        Selectable next = null;
+        if (direction == Vector2.up)
+            next = currentSelectable.FindSelectableOnUp();
+        else if (direction == Vector2.down)
+            next = currentSelectable.FindSelectableOnDown();
+        else if (direction == Vector2.left)
+            next = currentSelectable.FindSelectableOnLeft();
+        else if (direction == Vector2.right)
+            next = currentSelectable.FindSelectableOnRight();
+
+        return next != null ? next.gameObject : current;
+    }  
 }
