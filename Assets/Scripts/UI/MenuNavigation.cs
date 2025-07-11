cd5e6ee280e01acaa22433e77fdcd708177d453b
LanceCoolie2018 <Tmansornig@Gmail.com>
2025-07-11 15:35:32 -0400

    add Navigate function

--- Assets/Scripts/UI/MenuNavigation.cs
+++ Assets/Scripts/UI/MenuNavigation.cs
@@ -9,13 +9,34 @@ public class MenuNavigation : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void Navigate(Vector2 direction)
+    {
+        // Get the currently selected button
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(selectedObject);
+        }
+
+        // Find the next selectable object in the given direction
+        var current = EventSystem.current.currentSelectedGameObject;
+        if (current != null)
+        {
+            var next = FindNextSelectable(current, direction);
+            if (next != null)
+            {
+                EventSystem.current.SetSelectedGameObject(next);
+                selectedObject = next;
+            }
+        }
     }
 
     GameObject FindNextSelectable(GameObject current, Vector2 direction)
