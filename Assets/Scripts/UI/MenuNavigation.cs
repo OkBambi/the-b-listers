d1cbef6659d04d9111a16f9fbaec02bd59567f5b
LanceCoolie2018 <Tmansornig@Gmail.com>
2025-07-11 15:37:18 -0400

    add WASD and select logic to update and initiate in start

--- Assets/Scripts/UI/MenuNavigation.cs
+++ Assets/Scripts/UI/MenuNavigation.cs
@@ -9,13 +9,59 @@ public class MenuNavigation : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Check if this GameObject has a Button component
+        Button button = GetComponent<Button>();
+        if (button != null)
+        {
+            selectedObject = gameObject;
+        }
+        else
+        {
+            // Fallback: Find the first Button in the scene
+            Button fallbackButton = FindObjectOfType<Button>();
+            if (fallbackButton != null)
+            {
+                selectedObject = fallbackButton.gameObject;
+            }
+            else
+            {
+                Debug.LogWarning("No Button found in the scene.");
+            }
+        }
 
+        EventSystem.current.SetSelectedGameObject(selectedObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // WASD Navigation
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Navigate(Vector2.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Navigate(Vector2.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            Navigate(Vector2.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Navigate(Vector2.right);
+        }
 
+        // Enter to select
+        if (Input.GetKeyDown(KeyCode.Return) && selectedObject != null)
+        {
+            Button button = selectedObject.GetComponent<Button>();
+            if (button != null && button.interactable)
+            {
+                button.onClick.Invoke();
+            }
+        }
     }
 
     void Navigate(Vector2 direction)
