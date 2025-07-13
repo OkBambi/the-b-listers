0995c57f1eb5df824fa067e429b201ddcb7d4a25
Mortal-Atlas <jacobzgino@gmail.com>
2025-07-13 13:54:25 -0400

    Last Push before Code Freeze

---
+++ Assets/Scripts/Enemies/Monk/LockColorChangeStopWatch.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class LockColorChangeStopWatch : MonoBehaviour
+{
+
+    [SerializeField] StopWatch Stopwatch;
+    [SerializeField] ChainMarker[] ChainToggleables;
+    [SerializeField] RawImage[] ChainImageArray;
+    [SerializeField] PrimaryColor primaryColor;
+    [SerializeField] ColorSwapping ChangeColor;
+    [SerializeField] WaveColorLockMonk ColorLockMonk;
+
+
+    void Start()
+    {
+        primaryColor = Stopwatch.setColor;
+        //find ColorSwapping Script in scene
+        ChangeColor = FindFirstObjectByType<ColorSwapping>();
+        //Find the object with chain marker script on the scene
+        ChainToggleables = FindObjectsByType<ChainMarker>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        //add Images to ChainImageArray
+        ChainImageArray = new RawImage[2] { ChainToggleables[0].GetComponent<RawImage>(), ChainToggleables[1].GetComponent<RawImage>() };
+
+    }
+
+    public void SwapChainColor()
+    {
+        switch (primaryColor)
+        {
+            case PrimaryColor.RED:
+                ChainImageArray[0].color = Color.red;
+                ChainImageArray[1].color = Color.red;
+                ChangeColor.SwapToColour(GameManager.instance.playerScript.GetPlayerColor(), PrimaryColor.RED, ref GameManager.instance.playerScript.currentColor);
+                break;
+
+            case PrimaryColor.BLUE:
+                ChainImageArray[0].color = Color.blue;
+                ChainImageArray[1].color = Color.blue;
+                ChangeColor.SwapToColour(GameManager.instance.playerScript.GetPlayerColor(), PrimaryColor.BLUE, ref GameManager.instance.playerScript.currentColor);
+                break;
+
+            case PrimaryColor.YELLOW:
+                ChainImageArray[0].color = Color.yellow;
+                ChainImageArray[1].color = Color.yellow;
+                ChangeColor.SwapToColour(GameManager.instance.playerScript.GetPlayerColor(), PrimaryColor.YELLOW, ref GameManager.instance.playerScript.currentColor);
+                break;
+        }
+    }
+}
