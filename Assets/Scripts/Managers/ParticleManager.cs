95bb5affe7017bf79f23f8d655dc37ab07ceb132
BrandonCrespo <102543971+BrandonCrespo@users.noreply.github.com>
2025-07-13 02:46:35 -0400

    TEsts

--- Assets/Scripts/Managers/ParticleManager.cs
+++ Assets/Scripts/Managers/ParticleManager.cs
@@ -20,6 +20,7 @@ public class ParticleManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        Debug.Log("color spawn");
         colorParticles = Instantiate(colorParticlesGameObject, new Vector3(100, 100, 100), Quaternion.identity).GetComponent<ParticleSystem>();
         colorParticles.name = colorParticlesGameObject.name;
     }
