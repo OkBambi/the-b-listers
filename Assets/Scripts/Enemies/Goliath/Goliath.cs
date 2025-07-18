29b738ce2e85a667df259f0e14fa3b2d84f9c3f3
Mortal-Atlas <jacobzgino@gmail.com>
2025-07-18 14:42:34 -0400

    level boss edit sounds

--- Assets/Scripts/Enemies/Goliath/Goliath.cs
+++ Assets/Scripts/Enemies/Goliath/Goliath.cs
@@ -107,6 +107,7 @@ public class Goliath : EnemyBase
             if (stateTimer > topRoamTime)
             {
                 currentState = State.Diving;
+                AudioManager.instance.Play("Goliath_Dive");
                 roamTime = 0;
                 Debug.Log("Goliath is diving!");
             }
@@ -118,6 +119,7 @@ public class Goliath : EnemyBase
         }
         else if (currentState == State.Swimming)
         {
+
             Swimming();
         }
         else if (currentState == State.Breach)
@@ -147,6 +149,7 @@ public class Goliath : EnemyBase
     
     void Diving()
     {
+                    StartCoroutine(WhaleSplashsNoise());
         if (goliathHitLocation.transform.position == Vector3.zero)
         {
             Vector3 divePos = new Vector3(Random.Range(-1f, 1f) * mapRadius, 0.1f, Random.Range(-1f, 1f) * mapRadius);
@@ -154,7 +157,7 @@ public class Goliath : EnemyBase
             goliathHitLocation.GetComponent<Renderer>().enabled = true;
         }
 
-
+        StartCoroutine(WhaleSplashsNoise());
         stateTimer += Time.deltaTime;
 
         //go down to indicator and through the map
@@ -189,6 +192,7 @@ public class Goliath : EnemyBase
         Vector3 horizontalDirection = (playerRelativePos - transform.position).normalized;
 
         //jaws music...
+
         transform.Translate(horizontalDirection * swimSpeed * Time.deltaTime);
 
         stateTimer += Time.deltaTime;
@@ -203,7 +207,7 @@ public class Goliath : EnemyBase
     void Breach()
     {
         //UP.
-        
+        StartCoroutine(WhaleSplashsNoise());
         stateTimer += Time.deltaTime;
         if (stateTimer > timeBeforeBreach)
         {
@@ -229,6 +233,12 @@ public class Goliath : EnemyBase
 
             ColorSelection(setColor);
         }
+    }
+
+    IEnumerator WhaleSplashsNoise()
+    {
+        yield return new WaitForSeconds(0.1f);
+        AudioManager.instance.Play("Goliath_Splash");
     }
 
     public override void DeathCheck()
--- Assets/Scripts/Helpers/TextAnimations.cs
+++ Assets/Scripts/Helpers/TextAnimations.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public static class TextAnimations
 {
