ab01138afa10855e020008b8d0b0188d1c028ab2
LanceCoolie2018 <Tmansornig@Gmail.com>
2025-07-19 13:49:21 -0400

    clean PopOut

--- Assets/Scripts/UI/PopOut.cs
+++ Assets/Scripts/UI/PopOut.cs
@@ -6,23 +6,14 @@ public class PopOut : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     [SerializeField] Vector3 NormalPosition;
     [SerializeField] Vector3 HoverPosition;
 
-    void Awake()
-    {
-        //NormalPosition = transform.position;
-        //HoverPosition = NormalPosition + new Vector3(0f, 80f, 0f);
-    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        //moves the onject to the hover position when the mouse pointer enters
-        //transform.position = HoverPosition;
         transform.position += new Vector3(0f, 80f, 0f);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        //returns the object to its normal position when the mouse pointer exits
-        //transform.position = NormalPosition;
         transform.position -= new Vector3(0f, 80f, 0f);
     }
 
