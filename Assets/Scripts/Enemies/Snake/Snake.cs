7b5ff4896e816d4981fc4fd5ed37cd779fd6918d
LanceCoolie2018 <Tmansornig@Gmail.com>
2025-07-19 12:58:13 -0400

    clean snake script

--- Assets/Scripts/Enemies/Snake/Snake.cs
+++ Assets/Scripts/Enemies/Snake/Snake.cs
@@ -22,13 +22,11 @@ public class Snake : EnemyBase
 
     protected float timer;
     private Vector3 wanderingTarget;
-    //private bool isWandering = false;
 
     //FOLLOWING
     [SerializeField] protected Transform player;
     [SerializeField] protected float followRange = 10f;
 
-    //private bool isFollowing = false;
 
     //ATTACKING
 
@@ -43,7 +41,6 @@ public class Snake : EnemyBase
             int rand = Random.Range(0, colourIndexes.Count - 1);
             theBois[headIndex].setColor = (PrimaryColor)colourIndexes[rand];
 
-            Debug.Log((PrimaryColor)colourIndexes[rand]);
             colourIndexes.Remove(colourIndexes[rand]);
         }
 
@@ -99,29 +96,6 @@ public class Snake : EnemyBase
 
     }
 
-    //moves around the map
-    //void MoveTowards(Vector3 target)
-    //{
-    //    //Quaternion targetRotation = Quaternion.LookRotation(target - transform.position);
-    //    //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-    //    //transform.position = Vector3.MoveTowards(transform.position, target, movementSpeed * Time.deltaTime);
-    //    Vector3 direction = (target - transform.position);
-    //    direction.y = 0; //optional: keeps movement flat on the XZ plane
-    //    direction.Normalize();
-
-    //    Quaternion targetRotation = Quaternion.LookRotation(direction);
-    //    Quaternion newRotation = Quaternion.Slerp(rb.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-    //    rb.MoveRotation(newRotation);
-
-    //    Vector3 moveStep = direction * movementSpeed * Time.deltaTime;
-    //    rb.MovePosition(rb.position + moveStep);
-
-
-    //    if (Vector3.Distance(transform.position, target) < 0.5f)
-    //    {
-    //        isWandering = false; //stops wandering when close to target
-    //    }
-    //}
 
     void GetNewWanderTarget()
     {
@@ -137,24 +111,6 @@ public class Snake : EnemyBase
         NavMesh.SamplePosition(randomDirection, out navHit, dist, layermask);
         return navHit.position;
     }
-
-
-
-        //if (Vector3.Distance(Vector3.zero, randomDirection + transform.position) < EnemyManager.instance.stage.transform.localScale.x / 2f)
-        //{
-        //    randomDirection += transform.position;
-        //    wanderingTarget = new Vector3(randomDirection.x, transform.position.y, randomDirection.z);
-        //}
-        ////my thinking is to send a raycast down to make sure that that spot is okay
-        ////if (Physics.Raycast(randomDirection, -Vector3.up, 1f))
-        ////{
-        ////    randomDirection += transform.position;
-        ////    wanderingTarget = new Vector3(randomDirection.x, transform.position.y, randomDirection.z);
-        ////}
-        //else
-        //{
-        //    GetNewWanderTarget();
-        //}
 
     public override void takeDamage(PrimaryColor hitColor, int amount)
     {
